974a250ec044fc9f5b1491caf9aca13582cf5197
Gede Wahyu <tokekbesi@gmail.com>
2018-02-04 02:01:11 +0700

    register api

--- src/main/java/tokekbesi/TokekbesiApplication.java
+++ src/main/java/tokekbesi/TokekbesiApplication.java
@@ -3,27 +3,30 @@ package tokekbesi;
 import io.dropwizard.Application;
 import io.dropwizard.setup.Bootstrap;
 import io.dropwizard.setup.Environment;
+import tokekbesi.api.ChallengeAPI;
+import tokekbesi.impl.api.SlackChallengeAPI;
 
 public class TokekbesiApplication extends Application<TokekbesiConfiguration> {
 
-    public static void main(final String[] args) throws Exception {
-        new TokekbesiApplication().run(args);
-    }
+  public static void main(final String[] args) throws Exception {
+    new TokekbesiApplication().run(args);
+  }
 
-    @Override
-    public String getName() {
-        return "Tokekbesi";
-    }
+  @Override
+  public String getName() {
+    return "Tokekbesi";
+  }
 
-    @Override
-    public void initialize(final Bootstrap<TokekbesiConfiguration> bootstrap) {
-        // TODO: application initialization
-    }
+  @Override
+  public void initialize(final Bootstrap<TokekbesiConfiguration> bootstrap) {
+    // TODO: application initialization
+  }
 
-    @Override
-    public void run(final TokekbesiConfiguration configuration,
-                    final Environment environment) {
-        // TODO: implement application
-    }
+  @Override
+  public void run(final TokekbesiConfiguration configuration,
+      final Environment environment) {
+    ChallengeAPI challengeAPI = new SlackChallengeAPI();
+    environment.jersey().register(challengeAPI);
+  }
 
 }
